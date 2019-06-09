50d7cb0272c35b20ed0c54100b66f1b45a6e9570
clils <clils@192.168.0.3>
2019-06-09 14:45:23 +0900

    채팅

--- rise/news.php
+++ rise/news.php
@@ -12,3 +12,16 @@
         ?>
 </div>
 <script src="js/lightbox.js"></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5cfc9510267b2e5785317dfd/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
